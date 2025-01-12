37c626715fc5e7ffb13f03719fdfa0b2a2561495
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-12 18:28:26 +0530

    files

--- Object_literal/app.js
+++ Object_literal/app.js
@@ -12,3 +12,16 @@ const item = {
     discount: 20,
     colors: ["Red", "Blue", "Green", "Yellow"] //can give multiple values in single key in an object 
 };
+
+//How to access values from our object 
+// for example-> 2 ways are there to access values from an object
+//using square bracket
+// 1: student["name"]
+// student["age"]
+// student["marks"]
+
+// 2: Using DocumentTimeline(.) operator
+// item.price
+// item.discount
+// item.colors[0]
+// item.color[1]
