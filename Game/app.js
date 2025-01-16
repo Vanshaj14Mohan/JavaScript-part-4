565dc93953bbe7c0d538ba3328f721da79ff1e37
Vanshaj P Mohan <VanshajKumar145@gmail.com>
2025-01-16 23:32:59 +0530

    files

--- Game/app.js
+++ Game/app.js
@@ -10,12 +10,17 @@ while(true){
         console.log("You quit");
         break;
     }
-
     if(guess == random){
-        console.log("Congrats, Your guess was right.!");
+        console.log("Congrats, Your guess was right.! The random number was:", random);
         break;
     }
-    else{
-        guess = prompt("Your Guess was wrong, try again");
+    else if(guess < random) {
+        guess = prompt("Hint: Your guess was too small, Please try again");
     }
+    else if(guess > random){
+        guess = prompt("Hint: Your guess was too large, Please try again");
+    }
+    // else{
+    //     guess = prompt("Your Guess was wrong, try again");
+    // }
 }
